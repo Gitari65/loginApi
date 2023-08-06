9b506a4c20c5ffe60df4d08f7a039af30dba03a1
Gitari65 <agitari65@gmail.com>
2023-08-06 21:56:20 +0300

    servr changes

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 const express=require('express')
 const app=express()
-const  article =require('./data')
-app.get('/articles',(req,res)=>
+
+app.use(express.json())
+const persons=[]
+const bcrypt=require('bcrypt')
+app.get('/users',(req,res)=>
 {
-res.status(200).json(article);
+res.json(persons);
 })
-app.post('/login',(req,res)=>
+app.post('/users', async(req,res)=>
 {
+    try {
+    const passLogin= await bcrypt.hash(req.body.password,10)
+    console.log(passLogin)
+    const user={username:req.body.username,password:passLogin}
+     persons.push(user)
+    res.status(201).send([])
+        
+    } catch (error) {
+        console.log("error")
+        
+    }
+  
     
+
+})
+app.post('/users/login',async(req,res)=>
+{
+
+const user=persons.find(user=>user.name===req.body.name)
+if(user===null){
+    res.send("user not found")
+}
+try{
+   if(await bcrypt.compare(req.body.passLogin,passLogin)) 
+   {
+    res.send('successful login')
+   }
+   else{
+    res.send('')
+   }
+
+
+
+}
+catch(error){
+
+}
+
 })
 
 
-app.listen(3000)
+app.listen(5000)
